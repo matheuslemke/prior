855faa443952eaecdf6f1eb3c9c8fa4fd860a4d8
Anderson Lemke <lemke@taller.net.br>
2022-07-07 09:32:40 -0300

    feat: card initial

--- src/components/Area.tsx
+++ src/components/Area.tsx
@@ -4,8 +4,15 @@ type AreaProps = {
 }
 
 const Area = ({ title, bgColor }: AreaProps) => {
+  const onClick = (evt: any) => {
+    console.log('add a card', evt)
+  }
+
   return (
-    <div className={`h-[50vh] flex items-center justify-center font-sans ${bgColor}`}>
+    <div
+      className={`h-[50vh] flex items-center justify-center font-sans ${bgColor}`}
+      onClick={onClick}
+    >
       <div className="w-40 text-2xl">{title}</div>
     </div>
   )
