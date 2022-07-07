561a23499ec42f366f6e8ddf1646e3e5623b4bb4
Anderson Lemke <lemke@taller.net.br>
2022-07-07 09:14:34 -0300

    feat: area with color param

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,18 @@ import './App.css'
 import Area from './components/Area'
 
 const App = () => {
-  let titles = ['titulo 1', 'titulo 2', 'titulo 3', 'titulo 4']
+  let areas = [
+    { id: '1', title: 'Low effort + High impact', color: 'bg-green-400' },
+    { id: '2', title: 'High effort + High impact', color: 'bg-green-200' },
+    { id: '3', title: 'Low effort + Low impact', color: 'bg-lime-200' },
+    { id: '4', title: 'High effort + Low impact', color: 'bg-red-400' }
+  ]
 
   return (
     <div className="App grid grid-cols-2">
-      <Area title={titles[0]}></Area>
-      <Area title={titles[1]}></Area>
-      <Area title={titles[2]}></Area>
-      <Area title={titles[3]}></Area>
+      {areas.map((area) => (
+        <Area key={area.id} title={area.title} bgColor={area.color}></Area>
+      ))}
     </div>
   )
 }
--- src/components/Area.tsx
+++ src/components/Area.tsx
@@ -1,9 +1,14 @@
 type AreaProps = {
   title: string
+  bgColor: string
 }
 
-const Area = ({ title }: AreaProps) => {
-  return <div className="h-[50vh]">{title}</div>
+const Area = ({ title, bgColor }: AreaProps) => {
+  return (
+    <div className={`h-[50vh] flex items-center justify-center font-sans ${bgColor}`}>
+      <div>{title}</div>
+    </div>
+  )
 }
 
 export default Area
