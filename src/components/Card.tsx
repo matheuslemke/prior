f73a6eb981fd26535a0937e4e15f580986d27e50
Anderson Lemke <lemke@taller.net.br>
2022-07-12 08:50:24 -0300

    feat: add card component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,5 +1,13 @@
-const Card = () => {
-  return <div>Card</div>
+import { CSSProperties } from 'react'
+import { CardProps } from '../App'
+
+const Card = ({ content, position }: CardProps) => {
+  const style: CSSProperties = {
+    position: 'absolute',
+    top: `${position.top}px`,
+    left: `${position.left}px`
+  }
+  return <div style={style}>{content}</div>
 }
 
 export default Card
