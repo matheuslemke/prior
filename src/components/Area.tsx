63e39ae76dda7d8201d62e43a5bcdf7ce963a938
Anderson Lemke <lemke@taller.net.br>
2022-07-12 08:50:58 -0300

    refactor: remove gap from height calc

--- src/components/Area.tsx
+++ src/components/Area.tsx
@@ -1,16 +1,14 @@
+import { MouseEventHandler } from 'react'
+
 type AreaProps = {
   title: string
   bgColor: string
+  onClick: MouseEventHandler
 }
-
-const Area = ({ title, bgColor }: AreaProps) => {
-  const onClick = (evt: any) => {
-    console.log('add a card', evt)
-  }
-
+const Area = ({ title, bgColor, onClick }: AreaProps) => {
   return (
     <div
-      className={`h-[50vh] flex items-center justify-center font-sans ${bgColor}`}
+      className={`h-[calc(50vh_-_6px)] flex items-center justify-center font-sans ${bgColor}`}
       onClick={onClick}
     >
       <div className="w-40 text-2xl">{title}</div>
