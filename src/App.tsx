af20d3315a0822ebba4f341c1c12eeb79b0a317f
Anderson Lemke <lemke@taller.net.br>
2022-07-13 13:20:39 -0300

    refactor: change bg colors

--- src/App.tsx
+++ src/App.tsx
@@ -16,9 +16,9 @@ export type CardProps = {
 const App = () => {
   const areas = [
     { id: '1', title: 'Low effort + High impact', bgColor: 'bg-green-400' },
-    { id: '2', title: 'High effort + High impact', bgColor: 'bg-green-200' },
-    { id: '3', title: 'Low effort + Low impact', bgColor: 'bg-lime-200' },
-    { id: '4', title: 'High effort + Low impact', bgColor: 'bg-red-400' }
+    { id: '2', title: 'High effort + High impact', bgColor: 'bg-cyan-200' },
+    { id: '3', title: 'Low effort + Low impact', bgColor: 'bg-lime-300' },
+    { id: '4', title: 'High effort + Low impact', bgColor: 'bg-red-500' }
   ]
 
   const [cards, setCards] = useState<Array<CardProps>>([])
@@ -27,7 +27,7 @@ const App = () => {
     setCards([
       ...cards,
       {
-        content: 'novo',
+        content: '',
         position: {
           top: evt.clientY,
           left: evt.clientX
