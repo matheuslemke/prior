502140710a22a96f9e70347bf624ac491e12bcd5
Anderson Lemke <lemke@taller.net.br>
2022-07-21 20:35:04 -0300

    feat: add drag event

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,26 +1,28 @@
-import { CSSProperties, MouseEventHandler } from 'react'
 import { Position } from '../App'
 
 type CardProps = {
   id: string
-  content: string
   position: Position
-  closeCard: any
+  onRemoveCard: any
+  onPositionChanged: any
 }
 
-const Card = ({ id, content, position, closeCard }: CardProps) => {
-  const style: CSSProperties = {
-    top: `${position.top}px`,
-    left: `${position.left}px`
-  }
-  const closeThisCard = () => {}
+const Card = ({ id, position, onRemoveCard, onPositionChanged }: CardProps) => {
   return (
     <div
+      draggable={true}
       className="absolute flex flex-col overflow-hidden resize w-48 h-24 border-2 shadow-lg border-neutral-600 rounded-xl"
-      style={style}
+      style={{
+        top: `${position.top}px`,
+        left: `${position.left}px`
+      }}
+      onDragEnd={(evt) => {
+        console.log('evt', evt)
+        onPositionChanged(id, { left: evt.pageX, top: evt.pageY })
+      }}
     >
       <div className="bg-neutral-900 text-right">
-        <button className="text-neutral-100 text-xs mr-2" onClick={() => closeCard(id)}>
+        <button className="text-neutral-100 text-xs mr-2" onClick={() => onRemoveCard(id)}>
           X
         </button>
       </div>
