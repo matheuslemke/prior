ba82d3b5fb9353616839e2939425b9d7077a1b67
Anderson Lemke <lemke@taller.net.br>
2022-07-26 10:39:43 -0300

    feat: set min height for card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -11,15 +11,12 @@ const Card = ({ id, position, onRemoveCard, onPositionChanged }: CardProps) => {
   return (
     <div
       draggable={true}
-      className="absolute flex flex-col overflow-hidden resize w-48 h-24 border-2 shadow-lg border-neutral-600 rounded-xl"
+      className="absolute flex flex-col overflow-hidden resize-y w-48 h-24 min-h-[55px] border-2 shadow-lg border-neutral-600 rounded-xl"
       style={{
         top: `${position.top}px`,
         left: `${position.left}px`
       }}
-      onDragEnd={(evt) => {
-        console.log('evt', evt)
-        onPositionChanged(id, { left: evt.pageX, top: evt.pageY })
-      }}
+      onDragEnd={(evt) => onPositionChanged(id, { left: evt.pageX, top: evt.pageY })}
     >
       <div className="bg-neutral-900 text-right">
         <button className="text-neutral-100 text-xs mr-2" onClick={() => onRemoveCard(id)}>
